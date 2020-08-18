5f604aed6f8e716ae54739ae25e832d9944b79df
GraceC67 <gacarlso@ucsd.edu>
2020-08-18 03:27:49 +0000

    fixed py get

--- pwned135.site/public_html/cgi-bin/py-get-echo.py
+++ pwned135.site/public_html/cgi-bin/py-get-echo.py
@@ -2,6 +2,7 @@
 
 import time
 import cgi
+import cgitb
 import json
 import os
 
@@ -12,7 +13,9 @@ print("<html><head><title>GET query string</title></head>\
 
 print("Raw query string: \n<br/><br/>", os.getenv("QUERY_STRING"))
 print("<table> Formatted Query String:")
-
+query = cgi.FieldStorage()
+for key in query.keys():
+   print("\n" + key + '=' + query.getvalue(key))
 print("</table>")
 
 print("</body>")
