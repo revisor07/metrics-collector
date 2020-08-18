8f7ef56083e515351a5562c6627fa9f0c04af054
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 13:51:46 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-get-echo.php
+++ pwned135.site/public_html/cgi-bin/php-get-echo.php
@@ -4,8 +4,9 @@
 <?php
 $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $parts = parse_url($url);
+$query = parse_str($parts['query'], $query);
 echo "test =" . $_GET['test'];
-print_r($parts);
+echo $query;
 
 ?>
 
