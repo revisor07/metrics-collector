5530c3a00d423b95b74e8cbf5aab8947226247ca
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 12:51:37 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-hello-html-world.php
+++ pwned135.site/public_html/cgi-bin/php-hello-html-world.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
 <?php
 date_default_timezone_set('America/Los_Angeles');
 $date = date('m/d/Y h:i:s a', time());
 $ip = $_SERVER['REMOTE_ADDR'];
 
-print "This page was generated with the PHP programming langauge"
 print "<h1> Hello Team Pwned </h1>";
+print "This page was generated with the PHP programming langauge";
 print "Current time: " . $date . "<br>";
 print "Your current ip address is: " . $ip . "<br>";
 
 ?>
-
-</body>
-</html>
