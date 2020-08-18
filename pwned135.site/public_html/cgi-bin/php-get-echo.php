c5b5314331cf7f1f5591699d2d8da9945da99508
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 13:08:24 -0700

    s

---
+++ pwned135.site/public_html/cgi-bin/php-get-echo.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-type: application/json');
+$title = "Team Pwned";
+$message = "This page was generated with the PHP programming langauge";
+$heading = "Hello, PHP";
+date_default_timezone_set('America/Los_Angeles');
+$date = date('m/d/Y h:i:s a', time());
+$ip = $_SERVER['REMOTE_ADDR'];
+$data = array('title' => $title, 'heading' => $heading, 'message' => $message, 'IP' => $ip, 'time' => $date);
+
+echo json_encode($data);
+
+?>
