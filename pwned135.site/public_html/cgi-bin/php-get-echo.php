5260401e58c7bf6a166bdd00290278bb29aca5e5
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 14:25:02 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-get-echo.php
+++ pwned135.site/public_html/cgi-bin/php-get-echo.php
@@ -1,16 +1,16 @@
-<html>
-<body>
+<!DOCTYPE html>
+<html><head><title>GET Request Echo</title>
+</head><body><h1 align="center">Get Request Echo</h1>
 
 <?php
 $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $parts = parse_url($url);
 parse_str($parts['query'], $query);
-#echo "test =" . $_GET['test'];
+echo $_SERVER['QUERY_STRING']
 foreach($query as $key => $value) {
     echo "$key  = $value <br>";
 }
 
 ?>
 
-</body>
 </html>
