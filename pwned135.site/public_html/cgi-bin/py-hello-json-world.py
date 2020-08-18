2ce09b7f06e31c2ea604161cf72cd58fe874b632
GraceC67 <gacarlso@ucsd.edu>
2020-08-18 07:41:23 +0000

    finished py up to sessioning - need to add source and doc

--- pwned135.site/public_html/cgi-bin/py-hello-json-world.py
+++ pwned135.site/public_html/cgi-bin/py-hello-json-world.py
@@ -5,6 +5,7 @@ import cgi
 import json
 import os
 
+print("Cache-Control: no-cache")
 print("Content-type: application/json\r\n\r\n")
 print("{\n\t\"message\": \"Hello World\",\n")
 print("\t\"date\":", time.ctime(), "\n")
