580352a70d0a7ead0f0800089ed2e1b045965951
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 11:50:53 -0700

    s

---
+++ pwned135.site/public_html/cgi-bin/php-hello-html-world.php
@@ -0,0 +1,13 @@
+<html>
+<head>	
+  <title>Hello Team Pwned </title>
+</head>
+  <body>
+    <h1 align=center>Hello Team Pwned Py</h1>
+
+    <br><font size=+1>Hello World</font></br>
+    <br>This program was generated at: ', time.ctime(), '</br>
+
+    <br>Your current IP address is: ', os.getenv("REMOTE_ADDR"), '</br>
+  </body>
+</html>
