48b079d64dbe6a527bb29b60daa0dbbe8736c3eb
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 15:56:27 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-env.php
+++ pwned135.site/public_html/cgi-bin/php-env.php
@@ -3,9 +3,16 @@
 </head><body><h1 align="center">Environment Variables</h1>
 <hr>
 <?php
+
 $env = getenv();
+print("<h2>Environment Variables:</h2>");
+
 foreach($env as $var => $value) {
-    echo "$var  = $value <br>";
+    echo "<b>$var</b>  = $value <br>";
+}
+print("<h2>Server Variables:</h2>");
+foreach($_SERVER as $var => $value) {
+    echo "<b>$var</b>  = $value <br>";
 }
 ?>
 </body>
