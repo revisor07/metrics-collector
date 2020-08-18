a3963bb8c9f3cd446bd0933d4af3e0efb60f94b3
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 12:18:36 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-hello-html-world.php
+++ pwned135.site/public_html/cgi-bin/php-hello-html-world.php
@@ -3,7 +3,7 @@ $date = date('m/d/Y h:i:s a', time());
 $ip = getenv("REMOTE_ADDR"); 
 
 print "<h1> Hello Team Pwned </h1>";
-print "Current time: " . $date . ;
+print "Current time: " . $date . "<br>";
 print "Your current ip address is: " . $ip . ;
 
 ?>
