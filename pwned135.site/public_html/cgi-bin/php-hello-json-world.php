8c9dc9c06a65733a3114166d68e98b9cfd2367b2
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 13:09:29 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-hello-json-world.php
+++ pwned135.site/public_html/cgi-bin/php-hello-json-world.php
@@ -1,10 +1,14 @@
-<html>
-<body>
-
 <?php
-echo "test =" . $_GET['test'];
-?>
+header('Content-type: application/json');
+$title = "Team Pwned";
+$message = "This page was generated with the PHP programming langauge";
+$heading = "Hello, PHP";
+date_default_timezone_set('America/Los_Angeles');
+$date = date('m/d/Y h:i:s a', time());
+$ip = $_SERVER['REMOTE_ADDR'];
+$data = array('title' => $title, 'heading' => $heading, 'message' => $message, 'IP' => $ip, 'time' => $date);
 
-</body>
-</html>
+echo json_encode($data);
+
+?>
 
