f5e1bf764d8ab03f77453a1e1cc0619e8c475f03
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 17:52:07 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-sessions-1.php
+++ pwned135.site/public_html/cgi-bin/php-sessions-1.php
@@ -11,7 +11,7 @@ session_start();
   <p><b>Name:</b> You do not have a name set</p>
   <br>
   <br>
-  <a href="/cgi-bin/perl-sessions-2.pl">Session Page 2</a><br>
+  <a href="/cgi-bin/perl-sessions-2.php">Session Page 2</a><br>
   <a href="/perl-cgiform.html">Perl CGI Form</a><br>
   <form style="margin-top:30px" action="/cgi-bin/php-destroy-session.php" method="get">
   <button type="submit">Destroy Session</button></form>
