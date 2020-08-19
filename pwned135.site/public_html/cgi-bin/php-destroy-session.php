54f213440af0267f375b4c8ea8214ae15ad4ccf2
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 19:54:56 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-destroy-session.php
+++ pwned135.site/public_html/cgi-bin/php-destroy-session.php
@@ -5,7 +5,16 @@
 
 <?php
 // remove all session variables
-setcookie("username", "", time() - 3600);
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
+#setcookie("username", "", time() - 3600);
 session_unset();
 
 // destroy the session
