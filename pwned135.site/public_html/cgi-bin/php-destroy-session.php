9fcf0fc88b8a47f4998d55dc7b2c5c6cc721355a
iiuzifov <iiuzifov@ucsd.edu>
2020-08-18 19:05:29 -0700

    s

--- pwned135.site/public_html/cgi-bin/php-destroy-session.php
+++ pwned135.site/public_html/cgi-bin/php-destroy-session.php
@@ -12,7 +12,7 @@ session_destroy();
 ?>
 
 <h1>Session Destroyed</h1>
-<a href=/cgi-bin/php-cgiform.php>Back to the PHP CGI Form</a><br />
+<a href=/cgi-bin/php-state-demo.php>Back to the PHP CGI Form</a><br />
 <a href=/cgi-bin/php-sessions-1.php>Back to Page 1</a><br />
 <a href=/cgi-bin/php-sessions-2.php>Back to Page 2</a>
 </body>
--- pwned135.site/public_html/cgi-bin/php-sessions-2.php
+++ pwned135.site/public_html/cgi-bin/php-sessions-2.php
@@ -17,7 +17,7 @@ session_start();
   }
   ?>
   <a href="/cgi-bin/php-sessions-1.php">Session Page 2</a><br>
-  <a href="/cgi-bin/php-cgiform.php">Perl CGI Form</a><br>
+  <a href="/cgi-bin/php-state-demo.php">Perl CGI Form</a><br>
   <form style="margin-top:30px" action="/cgi-bin/php-destroy-session.php" method="get">
   <button type="submit">Destroy Session</button></form>
 
