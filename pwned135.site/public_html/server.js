5f004f43452ecd3fac33fa98f23e8325134016a4
iiuzifov <iiuzifov@ucsd.edu>
2020-08-22 20:00:29 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -11,11 +11,39 @@ server.use(jsonServer.defaults());
 // Add custom routes
 server.get('/custom', function (req, res) { res.json({ msg: 'hello' }) })
 
+var connection = mysql.createConnection({
+    host : "localhost",
+    port: "3306",
+    user : "root",
+    password : "",
+    database : "logs",
+    multipleStatements: true
+});
+var test = {
+  "data": {
+    cookieEnabled: true,
+    innerHeight: "798",
+    innerWidth: "881",
+    language: "en-US",
+    outerHeight: "883",
+    outerWidth: "1392",
+    userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537"
+    }
+}
+
 var code = {
 	"warhead_id" : "95683",
 	"access_code" : "FRTS45W1"
 }
-server.get('/api/browsers', function (req, res) { res.json({ code }) })
+server.get('/api/browsers', function (req, res, body) { res.json({ code })
+  var data = JSON.parse(body);
+  var responseJson = JSON.stringify(data.response);
+
+  var query = connection.query('INSERT INTO metricName SET column=?', [responseJson], function(err, result) {
+    if(err) throw err;
+    console.log('data inserted');
+  });
+})
 
 // Returns an Express router
 var router = jsonServer.router('db.json');
