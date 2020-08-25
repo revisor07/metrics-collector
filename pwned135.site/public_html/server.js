d1177b34bb2030902343be7dc19533862e90ed15
iiuzifov <iiuzifov@ucsd.edu>
2020-08-25 16:23:41 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -40,7 +40,7 @@ var connection = mysql.createConnection({
 });
 
 server.get('/browsers', function(req, res, next) {
-	res.connection.query('SELECT * from initialBrowserData', function (error, results, fields) {
+	res.locals.connection.query('SELECT * from initialBrowserData', function (error, results, fields) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 	  		//If there is error, we send the error in the error section with 500 status
