9f0464abc808efd5f7af2591ac1a99a49f72c03f
iiuzifov <iiuzifov@ucsd.edu>
2020-08-25 16:27:50 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -32,7 +32,7 @@ var test = {
 
 var connection = mysql.createConnection({
     host : "localhost",
-    port: "3306",
+    //port: "3306",
     user : "root",
     password : "",
     database : "logs",
