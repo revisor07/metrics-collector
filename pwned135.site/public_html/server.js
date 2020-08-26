bc95da6dfd449935f9430088ee09f1eab655049d
GraceC67 <gacarlso@ucsd.edu>
2020-08-26 03:18:57 +0000

    added get and get/id routes for all metrics

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -43,6 +43,7 @@ connection.connect(function(err) {
   console.log('Mysql Connected...');
 });
 
+//get whole browser
 server.get('/browsers', function(req, res, next) {
 	connection.query('SELECT * from initialBrowserData', function (error, results, fields) {
 	  	if(error){
@@ -53,7 +54,271 @@ server.get('/browsers', function(req, res, next) {
   	});
 });
 
+//select by id #
+server.get('/browsers/:id', function(req, res, next) {
+	connection.query('SELECT * from initialBrowserData WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
 
+//select all of navigationTiming
+server.get('/navigation', function(req, res, next) {
+	connection.query('SELECT * from navigationTiming', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get navigationTiming by id #
+server.get('/navigation/:id', function(req, res, next) {
+	connection.query('SELECT * from navigationTiming WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get all of networkInformation
+server.get('/network', function(req, res, next) {
+	connection.query('SELECT * from networkInformation', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+server.get('/network/:id', function(req, res, next) {
+	connection.query('SELECT * from networkInformation  WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get all of storage
+server.get('/storage', function(req, res, next) {
+	connection.query('SELECT * from storageEstimate', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get storage by id
+server.get('/storage/:id', function(req, res, next) {
+	connection.query('SELECT * from storageEstimate WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get all of fp
+server.get('/fp', function(req, res, next) {
+	connection.query('SELECT * from fp', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get fp by id
+server.get('/fp/:id', function(req, res, next) {
+	connection.query('SELECT * from fp WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get all of fcp
+server.get('/fcp', function(req, res, next) {
+	connection.query('SELECT * from fcp', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get fcp by id
+server.get('/fcp/:id', function(req, res, next) {
+	connection.query('SELECT * from fcp WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get all of fid
+server.get('/fid', function(req, res, next) {
+	connection.query('SELECT * from fid', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get fid by id
+server.get('/fid/:id', function(req, res, next) {
+	connection.query('SELECT * from fid WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+
+//get all of lcp
+server.get('/lcp', function(req, res, next) {
+	connection.query('SELECT * from lcp', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get lcp by id
+server.get('/lcp/:id', function(req, res, next) {
+	connection.query('SELECT * from lcp WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get all of lcpFinal
+server.get('/lcpfinal', function(req, res, next) {
+	connection.query('SELECT * from lcpFinal', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get lcpFinal by id
+server.get('/lcpfinal/:id', function(req, res, next) {
+	connection.query('SELECT * from lcpfinal WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+
+//get all of cls
+server.get('/cls', function(req, res, next) {
+	connection.query('SELECT * from cls', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get cls by id
+server.get('/cls/:id', function(req, res, next) {
+	connection.query('SELECT * from cls WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get all of clsFinal
+server.get('/clsfinal', function(req, res, next) {
+	connection.query('SELECT * from clsFinal', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get clsFinal by id
+server.get('/clsfinal/:id', function(req, res, next) {
+	connection.query('SELECT * from clsFinal WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+
+//get all of tbt
+server.get('/tbt', function(req, res, next) {
+	connection.query('SELECT * from tbt', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
+
+//get tbt by id
+server.get('/tbt/:id', function(req, res, next) {
+	connection.query('SELECT * from tbt WHERE id=?', [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  	} else {
+  			res.send(JSON.stringify({results}));
+	  	}
+  	});
+});
 /*
 server.get('/api/browsers', function (req, res) { 
   res.json({ test })
