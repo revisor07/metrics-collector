12142246bc9efc10efcdd22a0e6b1be2865591ad
iiuzifov <iiuzifov@ucsd.edu>
2020-08-25 21:29:52 -0700

    s

--- pwned135.site/public_html/collector.js
+++ pwned135.site/public_html/collector.js
@@ -444,7 +444,7 @@ function reportPerf(measureName, data, customProperties = {}) {
       vitalsScore: getVitalsScore(measureName, data),
     });
     // TODO: send payload to endpoint
-    console.log(payload["initialBrowserData"]);
+    console.log(payload."initialBrowserData");
   });
 }
 
