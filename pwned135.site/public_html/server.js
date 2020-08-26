ff56643aba9fd89657a8dbd5087a513bfdb0dea9
iiuzifov <iiuzifov@ucsd.edu>
2020-08-26 14:16:32 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -85,7 +85,7 @@ server.post('/browser', function (req, res, next) {
 });
 */
 server.post('/browser', (req, res, next) => {
-  if (connection.query('INSERT INTO initialBrowserData(data, vitalsScore) VALUES (?, ?);', [req.body["data"], req.body["vitalsScore"]]) ){
+  if (connection.query('INSERT INTO initialBrowserData(data, vitalsScore) VALUES (?, ?);', [req.body, req.body]) ){
   	 res.status(200).json({
      message: req.body
     })
