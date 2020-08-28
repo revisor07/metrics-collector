9538be9aee094d543dfffa79a889d000a15728a7
iiuzifov <iiuzifov@ucsd.edu>
2020-08-27 18:10:05 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -32,7 +32,7 @@ server.get('/browser', function(req, res, next) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 	  	} else {
-  			res.send(JSON.parse(results));
+  			res.send(results);
 	  	}
   	});
 });
