6029ad382593a29a78890f8963ac632ddc30fe36
iiuzifov <iiuzifov@ucsd.edu>
2020-09-02 17:36:17 -0700

    s

--- reporting.pwned135.site/public_html/login.php
+++ reporting.pwned135.site/public_html/login.php
@@ -1,10 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "logs");
-
-if($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
+require_once "config.php";
 
 
 session_start();
