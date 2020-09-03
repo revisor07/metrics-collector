14edde18d473389d8f72dcce9ef86d4209c123a7
iiuzifov <iiuzifov@ucsd.edu>
2020-09-03 16:37:10 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -21,6 +21,7 @@ connection.connect(function(err) {
 
 
 server.get('/users', function(req, res) {
+  res.set();
   connection.query('SELECT id, username, email, PASSWORD(password) as password, admin FROM users', function(err, rows, fields) {
     if (err) throw err;
     res.send(rows);
