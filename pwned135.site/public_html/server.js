4ebc3b41008834b1379b0be8927733e3bcea6441
iiuzifov <iiuzifov@ucsd.edu>
2020-09-03 16:28:13 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -28,7 +28,7 @@ server.get('/users', function(req, res) {
 });
 
 server.post('/users', (req, res, next) => {
-  if (connection.query('INSERT INTO user SET ?', req.body) ){
+  if (connection.query('INSERT INTO users SET ?', req.body) ){
      res.status(200).json({
      message: req.body
     })
