129bcc6dbd6e6f357836a338ea40425b58951c1e
iiuzifov <iiuzifov@ucsd.edu>
2020-09-02 20:02:20 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -26,6 +26,18 @@ server.get('/users', function(req, res) {
     res.send(rows);
   });
 });
+server.post('/users', (req, res, next) => {
+  if (connection.query('INSERT INTO user VALUES (?, ?, ?, ?, ?);', req.body) ){
+     res.status(200).json({
+     message: req.body
+    })
+  }
+  else
+    throw error;
+});
+
+
+
 
 server.get('/logs', function(req, res) {
   connection.query('SELECT * FROM initialBrowserData', function(err, rows, fields) {
--- reporting.pwned135.site/public_html/users.php
+++ reporting.pwned135.site/public_html/users.php
@@ -18,12 +18,8 @@ if( !isset($_SESSION['auth']) || $_SESSION['auth'] != true || $_SESSION['admin']
   <hr>
   <h1>User Management Console</h1>
   <!--Grid Component-->
-  <!--
-  <zing-grid>
-    <zg-data src="https://pwned135.site/api/users"></zg-data>
-  </zing-grid>
--->
-  <zing-grid src="https://pwned135.site/api/users/" editor editor-controls></zing-grid>
+
+  <zing-grid src="https://pwned135.site/api/users/" editor editor-control></zing-grid>
 
 
 
