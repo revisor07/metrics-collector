a497f8cac6df037a45e9fcb2a3372fbd6bb8d505
iiuzifov <iiuzifov@ucsd.edu>
2020-09-03 20:38:06 -0700

    s

--- reporting.pwned135.site/public_html/index.php
+++ reporting.pwned135.site/public_html/index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 if( !isset($_SESSION['auth']) || $_SESSION['auth'] != true ){
-	header('Location: /login.php');
-	exit();
+  header('Location: /login.php');
+  exit();
 } ?>
 
 <html>
@@ -43,24 +43,23 @@ body {
 <div id="threeSeries"></div>
 <script>
 ibd = {}; //initialBrowserData
-nt = {}; //navigationTiming
-ids = [];
+cls = {};
+id = [];
+cls_data = [];
 innerHeights = [];
 innerWidths = [];
 dimensions = [];
 cookiesYes = 0;
-tbtData = [];
-exIds = [];
 //just to make the pie chart look pretty since everyone has them enabled, 100% doesnt look pretty
 cookiesNo = 0;
 async function getData() {
         try {
-	    var res = await fetch('https://pwned135.site/api/browser');
-	    var ex = await fetch('https://pwned135.site/api/tbt');
-	    var data = await res.json();
-	    var exData = await ex.json();
-	    ibd = data;         
-	    exIbd = exData;
+      var res = await fetch('https://pwned135.site/api/browser');
+      var res2 = await fetch('https://pwned135.site/api/cls');
+      var data1 = await res.json();
+      var data2= await res2.json();
+      ibd = data;         
+      cls = exData;
         } catch (err) {
             console.error(err.message);
         }
@@ -68,7 +67,6 @@ async function getData() {
 getData().then(() => {
   for (x in ibd){
     if(ibd[x].data != null){
-    ids.push(ibd[x].id)
     innerWidths.push(JSON.parse(ibd[x].data).innerWidth)
     innerHeights.push(JSON.parse(ibd[x].data).innerHeight)
     if(JSON.parse(ibd[x].data).cookieEnabled == true)
@@ -78,14 +76,15 @@ getData().then(() => {
     }
   }
 
-  for (x in exIbd){
-    if(exIbd[x].data != null){
-	tbtData.push(JSON.parse(exIbd[x].data))	  
+  for (x in cls){
+    if(cls[x].data != null){
+    id.push(cls[x].id)
+  cls_data.push(JSON.parse(exIbd[x].data))    
     }
   }
 
   for (i=0; i < innerHeights.length; i++){
-  	coord = [innerHeights[i], innerWidths[i]]
+    coord = [innerHeights[i], innerWidths[i]]
     dimensions.push(coord)
   }
 
@@ -97,18 +96,24 @@ getData().then(() => {
   zingchart.render({
     id: 'threeSeries',
     data: {
-      type: 'bar',
+      type: 'line',
+      'scale-x': {
+        label: { 
+          text: "recorded entries",
+        }
+      },
       'scale-y': {
         label: { 
-          text: "Data Transmitted",
-	  "height": "15px",
+          text: "Google Score",
+    "height": "15px",
         }
       },
       title: {
-        text: "Data Transmitted During TBT"
+        text: "Cumulative Layout Shift/User experience over time"
       },
       series: [
-        { values: tbtData}
+        { values: id },
+        { values: cls_data}
       ]
     }
   });
@@ -130,7 +135,7 @@ getData().then(() => {
       'scale-y': {
         label: { 
           text: "Inner Width",
-	  "height": "15px",
+    "height": "15px",
         }
       },
       title: {
@@ -151,13 +156,13 @@ getData().then(() => {
     data: {
       type: 'pie',
       labels: [
-	      // Label 1
+        // Label 1
         {
         text: "cookies enabled",
         "font-family": "Georgia",
         "font-size": "20",
         x: "20%",
-	y: "70%",
+  y: "70%",
         },
         {
         text: "cookies disabled",
@@ -173,7 +178,7 @@ getData().then(() => {
       series: [
         { 
           text: 'Cookies Enabled',
-	  values: [cookiesYes] },
+    values: [cookiesYes] },
         { 
           text: 'Cookies Disabled',
           values: [cookiesNo] },
@@ -192,4 +197,3 @@ getData().then(() => {
 
 </body>
 </html>
-
