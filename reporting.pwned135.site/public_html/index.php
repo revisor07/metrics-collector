db6b88a8d3a46e9dbe8e9ac2f1301565d8179dec
iiuzifov <iiuzifov@ucsd.edu>
2020-09-03 21:24:55 -0700

    s

--- reporting.pwned135.site/public_html/index.php
+++ reporting.pwned135.site/public_html/index.php
@@ -107,7 +107,7 @@ getData().then(() => {
       'scale-y': {
         label: { 
           text: "Google Score",
-          "height": "1px",
+          "height": "15px",
         }
       },
       title: {
