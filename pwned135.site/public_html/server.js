2ff2788a3ccee772fd6d67f1b3bbe6193ad206ed
iiuzifov <iiuzifov@ucsd.edu>
2020-09-03 17:44:21 -0700

    s

--- pwned135.site/public_html/server.js
+++ pwned135.site/public_html/server.js
@@ -29,7 +29,7 @@ server.get('/users', function(req, res) {
 });
 
 server.post('/users', (req, res, next) => {
-  if (connection.query('INSERT INTO users SET ? ', req.body["id"], req.body["username"], 
+  if (connection.query('INSERT INTO users VALUES (?,?,?,?,?) ', req.body["id"], req.body["username"], 
     req.body["email"], md5(req.body["password"]), req.body["admin"]) ){
      res.status(200).json(
      req.body
