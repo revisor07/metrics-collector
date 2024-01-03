b304c272805506e3bb43da09aaba1ddb9fdcfb21
Yuriy Efremov <revisor14@gmail.com>
2024-01-03 01:08:55 -0800

    s

--- public_html/login.php
+++ public_html/login.php
@@ -46,7 +46,7 @@ if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
 				$_SESSION["admin"] = true;
 			}
 
-			header("Location: /index.php");
+			header("Location: /reporting.php");
 			exit();
 		}
 	}
