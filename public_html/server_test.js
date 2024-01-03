013483c75cdf5a6d17b52c155e1f84f52c4f99ee
Yuriy Efremov <revisor14@gmail.com>
2024-01-02 22:11:43 -0800

    s

--- public_html/server_test.js
+++ public_html/server_test.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 // create a new MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'metrics_data'
