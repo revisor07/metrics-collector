2108a3fac83b6d28269cffe4eb60302ef8b9a95b
Yuriy Efremov <revisor14@gmail.com>
2024-01-04 00:29:04 -0800

    s

--- public_html/users.php
+++ public_html/users.php
@@ -35,16 +35,15 @@ if( !isset($_SESSION['auth']) || $_SESSION['auth'] != true || $_SESSION['admin']
   <footer>
     <script>
       let connection_data;
-      fetch('connections.json')
-      .then(response => response.json())
-      .then(data => {
-        connection_data = data;
-      })
-      .catch(error => console.error('Error loading connections.json:', error));
-
+      async function getConnData() {
+        connection_data_raw = await fetch('connections.json');
+        connection_data = await connection_data_raw.json();
+      }
+      getConnData().then(() => {
       let apiUrl = `${connection_data.protocol}://${connection_data.server}/api/users`;
       let zingGridElement = document.getElementById("usersDatabase");
       zingGridElement.setAttribute("src", apiUrl);
+      });
     </script>
   </footer>
 
