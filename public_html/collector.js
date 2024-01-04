95615d4c4dd1ed9f79494675922f266b260d786a
Yuriy Efremov <revisor14@gmail.com>
2024-01-03 23:24:46 -0800

    s

--- public_html/collector.js
+++ public_html/collector.js
@@ -476,7 +476,7 @@ function reportPerf(measureName, data, customProperties = {}) {
     var connection_data;
     if(name != ""){
       async function getData() {
-        fetch('connections.json')
+        await fetch('connections.json')
         .then(response => response.json())
         .then(data => {
           connection_data = data;
