810fda40c5ac1a2c1589520b5c379d921512c155
Yuriy Efremov <revisor14@gmail.com>
2024-01-03 23:22:32 -0800

    s

--- public_html/test.js
+++ public_html/test.js
@@ -3,6 +3,6 @@ fetch('connections.json')
 .then(response => response.json())
 .then(data => {
     connection_data = data;
+    console.log("SNAKE", connection_data)
 })
 .catch(error => console.error('Error loading connections.json:', error));
-console.log("SNAKE", connection_data)
