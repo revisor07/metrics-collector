2672de915dbeb038bc1167f325d83a642eae23da
Yuriy Efremov <revisor14@gmail.com>
2024-01-05 10:09:45 -0800

    w

--- public_html/logout.php
+++ public_html/logout.php
@@ -7,7 +7,7 @@
   <body>
     <div class = info_block_wrapper>
     <div class = info_block>
-    <h3>You have been successfully logged out</h3>
+    <h2>You have been successfully logged out</h2>
 
     <form action="login.php">
         <input type="submit" value="back to login" />
